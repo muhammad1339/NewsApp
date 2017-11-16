880da897619d49f607ad532e109e6d76229bc25a
Muhammad Ahmed <muhammadahmed1339@gmail.com>
2017-11-16 22:40:48 +0200

    handle small exception

--- app/src/main/java/com/proprog/newsapp/NewsListAdapter.java
+++ app/src/main/java/com/proprog/newsapp/NewsListAdapter.java
@@ -39,7 +39,7 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ItemVi
     }
 
     @Override
-    public void onBindViewHolder(final ItemViewHolder holder, int position) {
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
         News news = newses.get(position);
         String title = news.getWebTitle();
         String section = news.getSectionName();
